9ce17fc06636f5e3b5184f352c0429f6044225d0
seonjiwon <tommys915@gmail.com>
2025-04-09 14:36:04 +0900

    Hello Controller

--- src/main/java/com/seonjiwon/NewsSummarySpring/HelloController.java
+++ src/main/java/com/seonjiwon/NewsSummarySpring/HelloController.java
@@ -11,6 +11,6 @@ import java.util.Map;
 public class HelloController {
     @GetMapping("/hello")
     public Map<String, String> helloMessage() {
-        return Map.of("message", "프론트와 성공적으로 연결되었습니다.");
+        return Map.of("message", "Connect Complete.");
     }
 }
